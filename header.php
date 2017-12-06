837adfa33c18288498ea7359f0c3f8906475c2c9
cipurs <ross.taylor@live.com>
2017-12-06 23:03:36 +0000

    Added files via upload
    Initial upload of project at starting stage

---
+++ header.php
@@ -0,0 +1,13 @@
+<header class="header-container global-container content-wrapper" role='banner'>
+      <span class="header-logo  content-text-title"><a href="index.php">PAINTED WITH PIXELS</a></span>
+<!-- Start of navigation -->
+        <nav class="header-navigation" role='navigation'>
+                <ul class="header-navigation-ul content-text-title">
+                  <li class="header-navigation-navlink"><a title="Homepage" href="index.php">HOME</a></li>
+                  <li class="header-navigation-navlink"><a title="My Portfolio" href="portfolio.php">PORTFOLIO</a></li>
+                  <li class="header-navigation-navlink"><a title="My Blog" href="blog.php">BLOG</a></li>
+                  <li class="header-navigation-navlink"><a title="About Me" href="about.php">ABOUT</a></li>
+                  <li class="header-navigation-navlink"><a title="Contact Me" href="contact.php">CONTACT</a></li>
+                </ul>
+        </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta charset="utf-8">
+      <meta name="description" content="Professional portfolio of Ross Taylor, a creative UX designer from the small town of Rochdale.">
+      <meta name="keywords" content="Portfolio, Rochdale, Manchester, North West, UX, UX Designer, Designer">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name='theme-color' content='#000'> <!-- Browser theme colour for ANDROID -->
+      <link rel='icon' href='media/images/icon.png'><!-- Bowser Tab Icon -->
+      <link href="style.css" type="text/css" rel="stylesheet" />
+      <title>Ross Taylor | Creative UX Designer</title>
+  </head>
+  <body>
+  <div class="global-container">
+    <?php include('header.php'); ?>
+    <main class="content-wrapper">
+      <!-- Hero class -->
+      <div class="hero-content content-text-title">
+        <h1 class="hero-content-title">ROSS TAYLOR</h1>
+        <p class="hero-content-subtitle content-text-body">Techie, Gamer and true <span class="hero-subtitle-red">red devil</span>! :) </p>
+        <div class="hero-buttons">
+          <button class="hero-content-button hero-content-hero-button-download"></button>
+          <button class="hero-content-button hero-content-button-internal"></button>
+      </div>
+
+      </div>
+      <!-- End of Hero class -->
+    </main>
+    <?php include('footer.php'); ?>
+  </div>
+  </body>
+</html>
